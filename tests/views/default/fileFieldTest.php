3492fcddcbd9d8cb341c6e8bafb4c1cd1077ed01
Nick West <nickwest@uw.edu>
2018-03-14 15:01:38 -0700

    Added File field tests

---
+++ tests/views/default/fileFieldTest.php
@@ -0,0 +1,36 @@
+<?php namespace Nickwest\EloquentForms\Test\view;
+
+use Sunra\PhpSimple\HtmlDomParser;
+
+use Nickwest\EloquentForms\Field;
+
+use Nickwest\EloquentForms\Test\FieldViewTestCase;
+
+class fileFieldTest extends FieldViewTestCase
+{
+    protected $test_value = 'yoda.pdf';
+    protected $test_type = 'file';
+
+    // Run all basic tests
+
+    public function test_field_has_correct_value_attribute_when_changed()
+    {
+        $this->Field->attributes->value = $this->test_value;
+        $dom = HtmlDomParser::str_get_html($this->Field->makeView()->render());
+        $remove_button = current($dom->find($this->test_tag));
+        $file_link = current($dom->find('div[class=file-link]'));
+
+        $this->assertEquals('Remove', $remove_button->value);
+        $this->assertEquals('yoda.pdf', trim($file_link->plaintext));
+    }
+
+    public function test_remove_button_can_have_a_different_value()
+    {
+        $this->Field->attributes->value = $this->test_value;
+        $this->Field->file_delete_button_value = 'Obliterate';
+        $dom = HtmlDomParser::str_get_html($this->Field->makeView()->render());
+        $remove_button = current($dom->find($this->test_tag));
+
+        $this->assertEquals('Obliterate', $remove_button->value);
+    }
+}
