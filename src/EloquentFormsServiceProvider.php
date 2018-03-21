cb5457ac1fcbb806ba5d0c68fff542a7a6a4cd60
Nick West <nickwest@uw.edu>
2018-03-21 15:12:42 -0700

    refactored blade directive code

--- src/EloquentFormsServiceProvider.php
+++ src/EloquentFormsServiceProvider.php
@@ -25,39 +25,24 @@ class EloquentFormsServiceProvider extends ServiceProvider {
         $this->loadViewsFrom(__DIR__.'/views', DefaultTheme::getDefaultNamespace());
 
         Blade::directive('eloquentforms_include', function($expression) {
-            if(strpos($expression, ',') !== false) {
-                $view = substr($expression, 0, strpos($expression, ','));
-                $remainder = substr($expression, strpos($expression, ','));
-            } else {
-                $view = $expression;
-                $remainder = '';
-            }
-            $template = substr($view, strpos($view, '::')+2);
+            $view = (strpos($expression, ',') !== false ? substr($expression, 0, strpos($expression, ',')) : $expression);
 
             return '<?php if(View::exists('.$view.')){
                 echo $__env->make('.$expression.', array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render();
             }else{
-                echo $__env->make(\''.DefaultTheme::getDefaultNamespace().'::'.$template.$remainder.', array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render();
+                echo $__env->make(\''.DefaultTheme::getDefaultNamespace().'::'.substr($expression, strpos($expression, '::')+2).', array_except(get_defined_vars(), array(\'__data\', \'__path\')))->render();
             } ?>';
         });
 
         Blade::directive('eloquentforms_component', function($expression) {
-            if(strpos($expression, ',') !== false) {
-                $view = substr($expression, 0, strpos($expression, ','));
-                $remainder = substr($expression, strpos($expression, ','));
-            } else {
-                $view = $expression;
-                $remainder = '';
-            }
-            $template = substr($view, strpos($view, '::')+2);
+            $view = (strpos($expression, ',') !== false ? substr($expression, 0, strpos($expression, ',')) : $expression);
 
             return '<?php if(View::exists('.$view.')){
                 $__env->startComponent('.$expression.');
             }else{
-                $__env->startComponent(\''.DefaultTheme::getDefaultNamespace().'::'.$template.$remainder.');
+                $__env->startComponent(\''.DefaultTheme::getDefaultNamespace().'::'.substr($expression, strpos($expression, '::')+2).');
             } ?>';
         });
-
     }
 
     /**
