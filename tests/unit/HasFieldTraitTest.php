c1e20bf9b5e2aed873a2944265d3957c032b49cd
Nick West <nickwest@uw.edu>
2018-03-23 11:25:26 -0700

    Fixing TravisCI Test coverage report

--- tests/unit/HasFieldTraitTest.php
+++ tests/unit/HasFieldTraitTest.php
@@ -26,5 +26,108 @@ class HasFieldTraitTest extends TestCase
         $this->Form = $this->createComplexForm();
     }
 
+    public function test_addField_adds_a_field_to_the_form()
+    {
+        // Add the field
+        $this->Form->addField('my_ultra_unique_name');
+        $this->assertInstanceOf(Field::class, $this->Form->getField('my_ultra_unique_name'));
+    }
+
+    public function test_getField_throws_exception_on_invalid_field()
+    {
+        // Make sure there's no such field already and we get the proper exception
+        $this->expectException(InvalidFieldException::class);
+        $this->Form->getField('another_unique_name');
+    }
+
+    public function test_addFields_adds_many_fields_to_a_form()
+    {
+        $field_names = $this->getManyFieldNames(5);
+
+        $this->Form->addFields($field_names);
+
+        foreach($field_names as $field_name) {
+            $this->assertInstanceOf(Field::class, $this->Form->getField($field_name));
+        }
+    }
+
+    public function test_removeField_removes_a_field_from_the_form()
+    {
+        $field_name = current($this->fields)['name'];
+
+        // Remove it
+        $this->Form->removeField($field_name);
+
+        // Should not be set anymore
+        $this->assertFalse(isset($this->Form->{$field_name}));
+
+        // Trying to get this field should also now throw an exception
+        $this->expectException(InvalidFieldException::class);
+        $this->Form->getField($field_name);
+    }
+
+    public function test_removeFields_removes_many_fields_from_the_form()
+    {
+        foreach(array_rand($this->fields, 3) as $key){
+            $remove[] = $this->fields[$key]['name'];
+            unset($this->fields[$key]);
+        }
+
+        $this->Form->removeFields($remove);
+
+        // The others still exist
+        foreach($this->fields as $field) {
+            $this->assertInstanceOf(Field::class, $this->Form->{$field['name']});
+        }
+
+        // The ones we removed do not exist
+        foreach($remove as $field_name) {
+            $this->assertFalse($this->Form->isField($field_name));
+        }
+    }
+
+    public function test_isField_returns_existence_of_field()
+    {
+        foreach($this->fields as $field){
+            $this->assertTrue($this->Form->isField($field['name']));
+        }
+
+        $this->assertFalse($this->Form->isField('some_field_that_does_not_exist'));
+    }
+
+    public function test_magic_set_method_will_set_field_value()
+    {
+        foreach($this->fields as $field){
+            $this->Form->{$field['name']} = $field['value'];
+            $this->assertEquals($field['value'], $this->Form->{$field['name']}->attributes->value);
+        }
+    }
+
+    public function test_magic_set_method_will_throw_exception_on_invalid_field()
+    {
+        $this->expectException(InvalidFieldException::class);
+        $this->Form->notafieldnameatall = 'broken';
+    }
+
+    public function test_magic_get_method_will_get_field_object()
+    {
+        foreach($this->fields as $field){
+            $this->assertInstanceOf(\Nickwest\EloquentForms\Field::class, $this->Form->{$field['name']});
+        }
+    }
+
+    public function test_magic_get_method_will_throw_exception_on_invalid_field()
+    {
+        $this->expectException(InvalidFieldException::class);
+        $Field = $this->Form->notafieldnameatall;
+    }
+
+    public function test_magic_isset_method_returns_existence_of_field()
+    {
+        foreach($this->fields as $field){
+            $this->assertTrue(isset($this->Form->{$field['name']}));
+        }
+        $this->assertFalse(isset($this->Form->notafieldnameatall));
+    }
 
 }
