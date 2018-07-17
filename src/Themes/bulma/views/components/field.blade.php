38576ef98a4cd3b958cdb54f5ae38e9946343807
Nick West <nickwest@uw.edu>
2018-07-17 10:03:42 -0700

    added extra css classes to field div

--- src/Themes/bulma/views/components/field.blade.php
+++ src/Themes/bulma/views/components/field.blade.php
@@ -2,8 +2,8 @@
     @include($Field->getViewNamespace().'::components.inline-start')
 @endif
 
-<div id="field-{{ $Field->attributes->name.($Field->attributes->multi_key != '' ? '_'.$Field->attributes->multi_key : '') }}" class="type-{{ $Field->attributes->type }} field {{ (isset($Field->attributes->required) ? 'required ' : '') }}{{ $Field->container_class }}">
-	{!! $field_markup !!}
+<div id="field-{{ $Field->attributes->name.($Field->attributes->multi_key != '' ? '_'.$Field->attributes->multi_key : '') }}" class="type-{{ $Field->attributes->type }} field {{ (isset($Field->attributes->required) ? 'required ' : '') }}{{ $Field->container_class }} {{ $Field->attributes->type }} {{ (isset($display_only) ? 'display_only' : '') }}">
+        {!! $field_markup !!}
 </div>
 
 @if(!$Field->is_inline && $prev_inline)
